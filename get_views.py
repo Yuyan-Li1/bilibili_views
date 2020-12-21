88ee1cfb14f08a606c795cf5b3812a06191f1f85
Yuyan Li <yuyanli1211@gmail.com>
2020-12-21 14:06:59 +0800

    finished

---
+++ get_views.py
@@ -0,0 +1,16 @@
+from bilibili_api import video
+# put BVs in BV.txt
+file = open('BV.txt', 'r')
+while True:
+    bv = file.readline()
+    if not bv:
+        break
+    v = video.get_video_info(bvid=bv.rstrip())
+    title = v['title']
+    views = v['stat']['view']
+    likes = v['stat']['like']
+    comments = v['stat']['reply']
+    print(f"Title: {title}")
+    print(f"    Views: {views}")
+    print(f"    Likes: {likes}")
+    print(f"    Comments: {comments}")
